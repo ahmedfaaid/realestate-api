617f620b4eaad16e91d128cd15a373fa76cc2367
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-08-03 00:33:27 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 const port = 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
