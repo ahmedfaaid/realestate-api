8ebdcbc21dc8b938512639f8c47eb004fe440069
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-08-11 23:52:47 -0400

    initial seeding

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+  
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from '../../App.module';
+import { SeederService } from './Seeders.service';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const seedersService = app.get(SeederService);
+  await seedersService.seedAll();
+  app.close();
+
+}
+bootstrap();
