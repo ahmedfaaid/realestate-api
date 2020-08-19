89d65fe5e79b8dddd3cb561fcb851f73815092c9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-08-18 21:43:36 -0400

    add image folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ListingModule } from './listing/listing.module';
 import { join } from 'path';
+import { ImageResolver } from './image/image.resolver';
+import { ImageService } from './image/image.service';
+import { ImageModule } from './image/image.module';
 
 @Module({
   imports: [
@@ -13,6 +16,8 @@ import { join } from 'path';
     }),
     TypeOrmModule.forRoot(),
     ListingModule,
+    ImageModule,
   ],
+  providers: [ImageResolver, ImageService],
 })
 export class AppModule {}
